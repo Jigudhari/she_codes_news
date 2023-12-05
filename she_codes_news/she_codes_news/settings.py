44b6ff0b310be43dc6ee35d57e56e155f805cca3
JIGNASA PATEL <jigudhari_23@yahoo.com>
2023-12-05 17:42:40 +0800

    before project

--- she_codes_news/she_codes_news/settings.py
+++ she_codes_news/she_codes_news/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     'users.apps.UsersConfig'
 ]
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -125,3 +127,5 @@ STATICFILES_DIRS = (
     )
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'news:index'
+LOGIN_REDIRECT_URL = 'news:index'
